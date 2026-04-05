6e5426ef8a6078ec8e61c2d8608335278ab0a27c
tuprikov <tuprikov@gmail.com>
2026-04-05 17:46:51 +0200

    Add form submission handling to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 const App = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <form>
+            <form onSubmit={(event) => {
+                event.preventDefault();
+                console.log('Hello React Form!');
+            }}>
                 <div className="flex flex-col gap-2">
                     <label htmlFor="name">Name</label>
-                    <input id="name" type="text" placeholder="Enter name" className="border border-gray-200 px-4 py-2 rounded-xl" />
+                    <input
+                        id="name"
+                        type="text"
+                        placeholder="Enter name"
+                        className="border border-gray-200 px-4 py-2 rounded-xl"
+                    />
+                    <button className="p-4 px-6 mt-4 bg-gray-800 text-white rounded-xl">Save</button>
                 </div>
             </form>
         </div>
